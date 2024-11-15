f443dcb5296f22a41f1a58e2d12a51af4395a516
berquerant <sin@sin-pro.local>
2024-11-16 00:04:06 +0900

    Add examples

---
+++ example_merger_test.go
@@ -0,0 +1,73 @@
+package structconfig_test
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"reflect"
+
+	"github.com/berquerant/structconfig"
+)
+
+func ExampleMerger() {
+	type T struct {
+		I  int    `default:"1"`
+		S  string `default:"s"`
+		II []int  `default:"[1]"`
+	}
+
+	callback := func(s structconfig.StructField, v string, fv func() reflect.Value) error {
+		if s.Name() != "II" {
+			return errors.New("unexpected field name")
+		}
+		var xs []int
+		if err := json.Unmarshal([]byte(v), &xs); err != nil {
+			return err
+		}
+		fv().Set(reflect.ValueOf(xs))
+		return nil
+	}
+
+	eq := func(a, b any) (bool, error) {
+		// expect only []int because int and string are supported by structconfig
+		xs, ok := a.([]int)
+		if !ok {
+			return false, nil
+		}
+		ys, ok := b.([]int)
+		if !ok {
+			return false, nil
+		}
+		if len(xs) != len(ys) {
+			return false, nil
+		}
+		for i, x := range xs {
+			if x != ys[i] {
+				return false, nil
+			}
+		}
+		return true, nil
+	}
+
+	m := structconfig.NewMerger[T](
+		structconfig.WithAnyCallback(callback),
+		structconfig.WithAnyEqual(eq),
+	)
+	got, err := m.Merge(
+		T{
+			I:  100,
+			S:  "s", // default
+			II: []int{100},
+		},
+		T{
+			I:  1, // default
+			S:  "win",
+			II: []int{1}, // default
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(got.I, got.S, got.II)
+	// Output: 100 win [100]
+}
---
+++ example_structconfig_test.go
@@ -0,0 +1,150 @@
+package structconfig_test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"reflect"
+	"sort"
+	"strings"
+
+	"github.com/berquerant/structconfig"
+	"github.com/spf13/pflag"
+)
+
+func ExampleStructConfig_FromDefault() {
+	type T struct {
+		B bool `default:"false"`
+		I int
+		F float32 `default:"1.1"`
+		S string  `default:"str"`
+	}
+
+	sc := structconfig.New[T]()
+	var got T
+	if err := sc.FromDefault(&got); err != nil {
+		panic(err)
+	}
+	fmt.Println(got.B, got.I, got.F, got.S)
+	// Output: false 0 1.1 str
+}
+
+func ExampleStructConfig_FromEnv() {
+	type T struct {
+		B  bool   `name:"bool_value"`
+		S  string `name:"string_value"`
+		N  int    `name:"int_value"`
+		N2 int
+		N3 int `name:"-"`
+	}
+
+	envs := map[string]string{
+		"BOOL_VALUE":   "true",
+		"STRING_VALUE": "str",
+	}
+	for k, v := range envs {
+		os.Setenv(k, v)
+	}
+	defer func() {
+		for k := range envs {
+			os.Unsetenv(k)
+		}
+	}()
+
+	sc := structconfig.New[T]()
+	var got T
+	if err := sc.FromEnv(&got); err != nil {
+		panic(err)
+	}
+	fmt.Println(got.B, got.S, got.N, got.N2, got.N3)
+	// Output: true str 0 0 0
+}
+
+func ExampleStructConfig_FromFlags() {
+	type T struct {
+		B       bool   `name:"bool_value" usage:"BOOL"`
+		S       string `name:"string_value" default:"str"`
+		Ignore1 int
+		Ignore2 int `name:"-"`
+		V       struct {
+			S string
+		} `name:"struct_value"`
+	}
+
+	anyCallback := func(s structconfig.StructField, v string, fv func() reflect.Value) error {
+		if x, ok := s.Tag().Name(); !ok || x != "struct_value" {
+			// among T, only struct_value is not supported
+			return errors.New("unexpected tag name")
+		}
+		fv().Set(reflect.ValueOf(struct {
+			S string
+		}{
+			S: v,
+		}))
+		return nil
+	}
+
+	fs := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	sc := structconfig.New[T](structconfig.WithAnyCallback(anyCallback))
+
+	if err := sc.SetFlags(fs); err != nil {
+		panic(err)
+	}
+
+	flagNames := []string{}
+	fs.VisitAll(func(f *pflag.Flag) {
+		flagNames = append(flagNames, f.Name)
+	})
+
+	if err := fs.Parse([]string{"--bool_value", "--struct_value", "sv"}); err != nil {
+		panic(err)
+	}
+
+	var got T
+	if err := sc.FromFlags(&got, fs); err != nil {
+		panic(err)
+	}
+
+	sort.Strings(flagNames)
+	fmt.Println(strings.Join(flagNames, ","))
+	fmt.Println(got.B, got.S, got.Ignore1, got.Ignore2, got.V.S)
+	// Output:
+	// bool_value,string_value,struct_value
+	// true str 0 0 sv
+}
+
+func ExampleStructConfig_FromFlags_prefix() {
+	tagPrefix := "sc"
+	type T struct {
+		B bool   `scname:"bool_value" scusage:"BOOL"`
+		S string `scname:"string_value" scdefault:"str"`
+	}
+
+	fs := pflag.NewFlagSet("test", pflag.ContinueOnError)
+	sc := structconfig.New[T](structconfig.WithPrefix(tagPrefix))
+
+	if err := sc.SetFlags(fs); err != nil {
+		panic(err)
+	}
+
+	flagNames := []string{}
+	fs.VisitAll(func(f *pflag.Flag) {
+		flagNames = append(flagNames, f.Name)
+	})
+
+	if err := fs.Parse([]string{"--bool_value", "--string_value", "sv"}); err != nil {
+		panic(err)
+	}
+
+	var got T
+	if err := sc.FromFlags(&got, fs); err != nil {
+		panic(err)
+	}
+
+	sort.Strings(flagNames)
+	fmt.Println(strings.Join(flagNames, ","))
+	fmt.Println(got.B, got.S)
+	// Output:
+	// bool_value,string_value
+	// true sv
+}
